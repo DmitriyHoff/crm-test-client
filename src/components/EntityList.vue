18fd69985d3a2249b40d138027acf8f257262a6d
Dmitriy Hoff <goffdmitriy@gmail.com>
2024-10-13 19:54:27 +0200

    update styles

--- src/components/EntityList.vue
+++ src/components/EntityList.vue
@@ -7,21 +7,23 @@ defineProps<{
 </script>
 
 <template>
-    <table class="border-collapse border size-full">
-        <thead>
-            <tr class="bg-slate-100">
-                <th>Тип</th>
-                <th>id</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-if="!list.length">
-                <td colspan="2">Список пуст</td>
-            </tr>
-            <tr v-for="item in list" :key="item.id">
-                <td class="border px-2 py-1">{{ item.type }}</td>
-                <td class="border px-2 py-1">{{ item.id }}</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="rounded overflow-hidden size-full">
+        <table class="border-collapse border size-full">
+            <thead>
+                <tr class="bg-slate-100">
+                    <th>Тип</th>
+                    <th>id</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-if="!list.length">
+                    <td class="border px-2 py-1" colspan="2">Список пуст</td>
+                </tr>
+                <tr v-for="item in list" :key="item.id">
+                    <td class="border px-2 py-1">{{ item.type }}</td>
+                    <td class="border px-2 py-1">{{ item.id }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
